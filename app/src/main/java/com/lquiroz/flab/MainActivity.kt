c9b7861cb930270052c9e9071a1b54dfe23274ce
Lquiroz-edu <lquiroz20@alumno.uned.es>
2026-09-17 00:42:28 +0900

    Fix system theme import

--- app/src/main/java/com/lquiroz/flab/MainActivity.kt
+++ app/src/main/java/com/lquiroz/flab/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
